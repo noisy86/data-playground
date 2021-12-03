677bcf825322c1291044947ae758a5c6b1bb531b
noisy86 <57344673+noisy86@users.noreply.github.com>
2021-12-03 01:33:01 +0100

    hw

--- src/main/java/streams/DataProcessor.java
+++ src/main/java/streams/DataProcessor.java
@@ -3,37 +3,38 @@ package streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DataProcessor {
 
 
     public static boolean atLeastOneGradeA(Student student) {
-        return false;
+        return student.getGrades().stream().anyMatch(grade -> grade.getType() == GradeType.A);
     }
 
 
     public static List<Integer> getStudentAges(List<Student> students) {
-        return new ArrayList<>();
+        return students.stream().map(Student::getAge).collect(Collectors.toList());
     }
 
     public static List<Student> getStudentsWithMinimumAge(List<Student> students, int minAge) {
-        return new ArrayList<>();
+        return students.stream().filter(student -> student.getAge() >= minAge).collect(Collectors.toList());
     }
 
 
     // getGender() == Gender.MALE
     // or getGender().name().equals("MALE")
     public static long countMaleStudents(List<Student> students) {
-        return 0;
+        return students.stream().filter(student -> student.getGender() == Gender.MALE).count();
     }
 
 
     public static double avgAgeOfFemaleStudent(List<Student> students) {
-        return 0.0;
+        return students.stream().filter(student -> student.getGender() == Gender.FEMALE).mapToInt(Student::getAge).average().orElseThrow(IllegalStateException::new);
     }
 
     public static Integer getProductOfStudentAges(List<Student> students) {
-        return 0;
+        return students.stream().mapToInt(Student::getAge).reduce((student1, student2) -> student1 * student2).orElse(-1);
     }
 
     // ignore F Grades
