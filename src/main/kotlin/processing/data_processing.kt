9076cf9343b64ce7a724fbc688d128fb0d63aa3d
noisy86 <57344673+noisy86@users.noreply.github.com>
2022-01-06 21:01:06 +0100

    hw

--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -1,53 +1,53 @@
-package processing
+    package processing
 
-import streams.*
-
-
-fun atLeastOneGradeA(student: Student): Boolean {
-    return student.grades.any{ it.type == GradeType.A }
-}
+    import streams.*
 
 
-fun getStudentAges(students: List<Student>): List<Int> {
-    return students.map { it.age }
-}
-
-fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
-    return students.filter { it.age >= minAge }
-}
+    fun atLeastOneGradeA(student: Student): Boolean {
+        return student.grades.any{ it.type == GradeType.A }
+    }
 
 
-// gender == Gender.MALE
-// or gender.name == "MALE"
-fun countMaleStudents(students: List<Student>): Int {
-    return students.filter { it.gender == Gender.MALE }.count()
-}
+    fun getStudentAges(students: List<Student>): List<Int> {
+        return students.map { it.age }
+    }
+
+    fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
+        return students.filter { it.age >= minAge }
+    }
 
 
-// gender == Gender.FEMALE
-// or gender.name == "FEMALE"
-fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    return students.filter { it.gender == Gender.FEMALE }.map { it.age }.average()
-}
+    // gender == Gender.MALE
+    // or gender.name == "MALE"
+    fun countMaleStudents(students: List<Student>): Int {
+        return students.filter { it.gender == Gender.MALE }.count()
+    }
 
-fun getProductOfStudentAges(students: List<Student>): Int {
-    return students.map { it.age }.reduce { it1, it2 -> it1 * it2}
-}
 
-// ignore F Grades
-fun productOfStudentGrades(student: Student): Int {
-    return student.grades.filter { it.type != GradeType.F}.map{it.type.value}.reduce {it1, it2 -> it1 * it2}
-}
+    // gender == Gender.FEMALE
+    // or gender.name == "FEMALE"
+    fun avgAgeOfFemaleStudent(students: List<Student>): Double {
+        return students.filter { it.gender == Gender.FEMALE }.map { it.age }.average()
+    }
 
-// region BONUS
+    fun getProductOfStudentAges(students: List<Student>): Int {
+        return students.map { it.age }.reduce { it1, it2 -> it1 * it2}
+    }
 
-// use maxByOrNull on grades
-fun getBestMathGradeFromStudent(student: Student): Grade? {
-    return student.grades.filter { it.subject == Subject.MATH }.maxByOrNull { it }
-}
+    // ignore F Grades
+    fun productOfStudentGrades(student: Student): Int {
+        return student.grades.filter { it.type != GradeType.F}.map{it.type.value}.reduce {it1, it2 -> it1 * it2}
+    }
 
-fun getSortedAges(students: List<Student>): List<Int> {
-    return students.map { it.age }.sorted()
-}
+    // region BONUS
 
-// endregion
+    // use maxByOrNull on grades
+    fun getBestMathGradeFromStudent(student: Student): Grade? {
+        return student.grades.filter { it.subject == Subject.MATH }.maxByOrNull { it }
+    }
+
+    fun getSortedAges(students: List<Student>): List<Int> {
+        return students.map { it.age }.sorted()
+    }
+
+    // endregion
