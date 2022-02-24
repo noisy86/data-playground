a686e30317dcd44a38cdf9b5a353d503de0cb77a
noisy86 <57344673+noisy86@users.noreply.github.com>
2022-02-24 20:44:17 +0100

    DU

--- src/main/kotlin/arrlist1/Main.kt
+++ src/main/kotlin/arrlist1/Main.kt
@@ -2,13 +2,13 @@ package arrlist1
 
 fun main() {
 
-    val arrayList = ArrList(arrayOf(5,5,5,4,2))
+    val arrayList = ArrList(arrayOf(7,3,3,4,12))
     println(arrayList.size())
     println(arrayList.isEmpty())
-    println(arrayList.indexOf(2))
+    println(arrayList.indexOf(3))
     println(arrayList.toArr())
-    println(arrayList.get(4))
-    arrayList.add(6)
+    println(arrayList.get(1))
+    arrayList.add(8)
     arrayList.set(0, 2)
     println(arrayList.toArr().contentToString())
     println(arrayList.ifIn(5))
